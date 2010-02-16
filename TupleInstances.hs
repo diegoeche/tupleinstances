32fe6d98861ed52d88e62b4f3ecff46497be37c2
Diego Echeverri <diegoeche@gmail.com>
2010-02-16 13:30:55 -0500

    Added T class and a first experiment

--- TupleInstances.hs
+++ TupleInstances.hs
@@ -1,4 +1,7 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TemplateHaskell, 
+             MultiParamTypeClasses, 
+             FlexibleInstances,
+             FunctionalDependencies #-}
 module Data.Tuple.Instances where
 
 import THMacros
@@ -9,8 +12,17 @@ $(return $ map unTn [2..15])
 
 $(return $ map functorTn [2..15])
 
+class T a b | a -> b,  b -> a where
+      unT   :: a -> b
+      t     :: b -> a
 
+instance T (T3 a) (a, a, a) where
+         t = T3
+         unT = unT3
 
+-- example :: (Num a) => (a, a, a)
+example1 = (unT . fmap (+1) $ T3 (1,2,3)) 
 
-
-
+-- This one needs a explicit signature
+example2 :: (Num a) => (a, a, a)
+example2 = (unT . fmap (+1) $ t (1,2,3)) 
